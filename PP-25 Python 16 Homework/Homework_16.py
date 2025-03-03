73d0eadf3158d56c7a5f56c60b5b086bb22cb90d
levan-Gafrindashvili <levani.gafrindashvili9@gmail.com>
2025-03-03 16:52:16 +0400

    HomeWork 16

--- PP-25 Python 16 Homework/Homework_16.py
+++ PP-25 Python 16 Homework/Homework_16.py
@@ -14,33 +14,46 @@
 
 # 1)
 
-def transaction_fee(func):
-    def wrapper(balance, amount):
-        fee = 1
-        total = amount + fee
-        if balance < total:
-            return "Not Enough funds"  
+# def transaction_fee(func):
+#     def wrapper(balance, amount):
+#         fee = 1
+#         total = amount + fee
+#         if balance < total:
+#             return "Not Enough funds"  
         
-        return func(balance - total, amount)
-    return wrapper
+#         return func(balance - total, amount)
+#     return wrapper
 
-@transaction_fee
-def transaction_proccess(balance, amount):
-    return f"Transaction completed, Blance Left: {balance}"
+# @transaction_fee
+# def transaction_proccess(balance, amount):
+#     return f"Transaction completed, Blance Left: {balance}"
 
-balance = 100
-amount = 50
-print(transaction_proccess(balance, amount))
+# balance = 100
+# amount = 50
+# print(transaction_proccess(balance, amount))
 
-balance = 10
-amount = 8
-print(transaction_proccess(balance, amount))
+# balance = 10
+# amount = 8
+# print(transaction_proccess(balance, amount))
 
 # 2)
 
-class MetaClass:
-    def __init__(cls, name, bases, class_dict):
+class MetaClass(type):
+    def __new__(cls, name, bases, class_dict):
         for attr_name, attr_value in class_dict.items():
-            if callable(attr_value):
-                raise ValueError ("Method name can't start with _")
-        return super().__init__(name, bases, class_dict)
+            if callable(attr_value) and not attr_name.startswith("_"):
+                raise ValueError("Method name must start with _")
+        return super().__new__(cls, name, bases, class_dict)
+
+class ValidClass(metaclass=MetaClass):
+    def _valid_method(self): pass
+
+try:
+    class InvalidClass(metaclass=MetaClass):
+        def invalid_method(self): pass
+except ValueError as e:
+    print(e)
+
+# ეს როგორ დავწერე არ ვიცი სწორი თუა ეგეც არ ვიცი 
+# მერე ერთი ორჯერ კიდე გადავხედავ ამ ლექციას რო უკეთ გავიგო 
+# რა დონეზეც გავიგე ასე დავწერე
